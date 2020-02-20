a838615caaf26748b43a52714884ca31d49ad71a
Christopher Holley <asiholley@gmail.com>
2020-02-20 14:08:17 -0800

    javascript project fubar

---
+++ Web_Fund/CSS/functionpractice.js
@@ -0,0 +1,19 @@
+function psrl(tarr) {
+    var sml = tarr[0]
+    var lrg = tarr[0]
+    for(var i = 0; i < tarr.length; i++){
+        if(sml > tarr[i]){
+            sml = tarr[i];
+        }
+        if(lrg < tarr[i]){
+            lrg = tarr[i];
+        }
+    }
+    
+    console.log(sml);
+    return lrg;
+}
+var arr = [8,3,2,5,7]
+var large = psrl(arr)
+
+console.log(large)
