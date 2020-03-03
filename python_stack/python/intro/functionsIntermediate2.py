ec987ef759f2e6ed14e7a21a3bbe76cd4e7e487a
Christopher Holley <asiholley@gmail.com>
2020-03-03 10:16:40 -0800

    functions intermediate 2

---
+++ python_stack/python/intro/functionsIntermediate2.py
@@ -0,0 +1,116 @@
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+
+# Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
+x[1][0] = 15 
+print(x)
+
+# Change the last_name of the first student from 'Jordan' to 'Bryant' 
+students[0]['last_name'] = 'Bryant'
+print(students[0]['last_name'])
+
+# In the sports_directory, change 'Messi' to 'Andres'
+sports_directory['soccer'][0] = 'Andres'
+print(sports_directory['soccer'][0])
+
+# Change the value 20 in z to 30
+z[0]["y"] = 30
+print(z[0]["y"])
+
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)copy
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
+
+
+def iterateDictionary(list):
+    # for y in list:
+    #     print(y)
+    for i in range(len(students)):
+        print("first_name - " + students[i]['first_name'] + ", last_name -"+ students[i]['last_name'])
+        # print(students[i].keys())
+        # for key in students[i].keys():
+        #     print(key)
+        #     print(students[i][key])
+
+
+iterateDictionary(students) 
+
+
+# Create a function iterateDictionary2(key_name, some_list) that, given a list of dictionaries and a key name, the function prints the value stored in that key for each dictionary. For example, iterateDictionary2('first_name', students) should output:
+# Michael
+# John
+# Mark
+# KB
+# copy
+# And iterateDictionary2('last_name', students) should output:
+
+# Jordan
+# Rosales
+# Guillen
+# Tonel
+
+def iterateDictionary2(key_name, some_list):
+    for i in range(len(some_list)):
+        print(some_list[i][key_name])
+        
+
+iterateDictionary2('first_name', students)
+iterateDictionary2('last_name', students)
+
+
+# Create a function printInfo(some_dict) that given a dictionary whose values are all lists, prints the name of each key along with the size of its list, and then prints the associated values within each key's list. For example:
+
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+def printInfo(some_list):
+    # for i in range(len(some_list)):
+    #     print(len(some_list))
+    for a in some_list:
+        print(len(some_list[a]), a)
+        for b in some_list[a]:
+            print(b) 
+            
+print(dojo['locations'])
+
+
+printInfo(dojo)
+# # output:
+# 7 LOCATIONS
+# San Jose
+# Seattle
+# Dallas
+# Chicago
+# Tulsa
+# DC
+# Burbank
+    
+# 8 INSTRUCTORS
+# Michael
+# Amy
+# Eduardo
+# Josh
+# Graham
+# Patrick
+# Minh
+# Devon
