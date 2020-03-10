61fc11b1b165fe102f4ff91d6db684440a2fd706
Christopher Holley <asiholley@gmail.com>
2020-03-10 13:31:53 -0700

    add SQL to ORM

--- python_stack/django/django_intro/databaseProj/databasePracticeApp/models.py
+++ python_stack/django/django_intro/databaseProj/databasePracticeApp/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 # Create your models here.
-class Movie(models.Model):
-    title = models.CharField(max_length=45)
-    description = models.TextField()
-    release_date = models.DateTimeField()
-    duration = models.IntegerField()
+class Wizard(models.Model):
+    name = models.CharField(max_length=45)
+    house = models.CharField(max_length=45)
+    pet = models.CharField(max_length=45)
+    year = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
-    def __repr__(self):
-        return "Title: {}".format(self.title)
+    # def __repr__(self):
+    #     return "Title: {}".format(self.title)
