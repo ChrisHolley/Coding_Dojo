89025cc25c9df19e67fc19d13ab3d598a07bc00f
Christopher Holley <asiholley@gmail.com>
2020-03-10 16:58:38 -0700

    dojos & ninjas shell portion complete

--- python_stack/django/django_intro/databaseProj/databasePracticeApp/models.py
+++ python_stack/django/django_intro/databaseProj/databasePracticeApp/models.py
@@ -1,13 +1,31 @@
 from django.db import models
 
 # Create your models here.
-class users(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email_address = models.CharField(max_length=255)
-    age = models.IntegerField()
+# class users(models.Model):
+#     first_name = models.CharField(max_length=255)
+#     last_name = models.CharField(max_length=255)
+#     email_address = models.CharField(max_length=255)
+#     age = models.IntegerField()
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     update_at = models.DateTimeField(auto_now=True)
+
+
+# Dojo & Ninjas (shell)
+class dojos(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=2)
+    desc = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
-    def __repr__(self):
-        return "first_name: {}".format(self.first_name)
+class ninjas(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    dojo = models.ForeignKey(dojos, related_name="ninjas", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+# Dojo & Ninjas (shell) END
+
+    # def __repr__(self):
+    #     return "first_name: {}".format(self.first_name)
