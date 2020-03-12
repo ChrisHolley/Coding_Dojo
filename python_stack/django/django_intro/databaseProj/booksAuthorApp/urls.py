b5c466c4306cba47dc27f3200765fb494f22b028
Christopher Holley <asiholley@gmail.com>
2020-03-11 19:34:50 -0700

    add django ORM book & authors HTML

--- python_stack/django/django_intro/databaseProj/booksAuthorApp/urls.py
+++ python_stack/django/django_intro/databaseProj/booksAuthorApp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.books),
+    path('', views.bookpage),
+    path('addBook', views.addBook),
+    path('<int:bookID>', views.viewBook)
 ]
--- python_stack/django/django_intro/databaseProj/booksAuthorApp/views.py
+++ python_stack/django/django_intro/databaseProj/booksAuthorApp/views.py
@@ -1,29 +1,50 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .models import *
-def books(request):
-    return HttpResponse("test page for books/")
-def displayFruit(request):
-    return render(request, "fruits.html")
-def checkout(request):
-    print("Got Post Info*****************************")
-    print(request.POST)
-    # 'count': sum(strawberry + raspberry + apple)
-    request.session['item_counter'] = int(request.POST['strawberry']) + int(request.POST['raspberry']) + int(request.POST['apple'])
-    context= {
-        "strawberry": request.POST['strawberry'],
-        "raspberry": request.POST['raspberry'],
-        "apple": int(request.POST['apple']),
-        "firstName": request.POST['first_name'],
-        "lastName": request.POST['last_name'],
-        "student_id": request.POST['student_id'],
+
+def bookpage(request):
+    context = {
+        "books": books.objects.all(),
+        "authors": authors.objects.all()
     }
-    first_name = context['firstName']
-    last_name = context['lastName']
-    raspberry = int(context['raspberry'])
-    apple = int(context['apple'])
-    strawberry = int(context['strawberry'])
-    count_int = apple + strawberry + raspberry
-    count = int(raspberry)+int(apple)+int(strawberry)
-    print(f"Charging {first_name} {last_name} for {count_int} {count}  {int(strawberry)+int(raspberry)+int(apple)} items ")
-    return render(request, "checkout.html", context, count)
+    return render(request, "books.html", context)
+
+def addBook(request):
+    title = request.POST['book_title']
+    desc = request.POST['book_desc']
+    books.objects.create(title=title, desc=desc)
+    return redirect('/books')
+
+def viewBook(request, bookID):
+    this_book = books.objects.get(id=bookID)
+    context = {
+        "thisBook": books.objects.get(id=bookID),
+        "bookAuthors": this_book.Authors.all()
+    }
+    return render(request, "viewBook.html", context)
+
+
+# def displayFruit(request):
+#     return render(request, "fruits.html")
+# def checkout(request):
+#     print("Got Post Info*****************************")
+#     print(request.POST)
+#     # 'count': sum(strawberry + raspberry + apple)
+#     request.session['item_counter'] = int(request.POST['strawberry']) + int(request.POST['raspberry']) + int(request.POST['apple'])
+#     context= {
+#         "strawberry": request.POST['strawberry'],
+#         "raspberry": request.POST['raspberry'],
+#         "apple": int(request.POST['apple']),
+#         "firstName": request.POST['first_name'],
+#         "lastName": request.POST['last_name'],
+#         "student_id": request.POST['student_id'],
+#     }
+#     first_name = context['firstName']
+#     last_name = context['lastName']
+#     raspberry = int(context['raspberry'])
+#     apple = int(context['apple'])
+#     strawberry = int(context['strawberry'])
+#     count_int = apple + strawberry + raspberry
+#     count = int(raspberry)+int(apple)+int(strawberry)
+#     print(f"Charging {first_name} {last_name} for {count_int} {count}  {int(strawberry)+int(raspberry)+int(apple)} items ")
+#     return render(request, "checkout.html", context, count)
 
