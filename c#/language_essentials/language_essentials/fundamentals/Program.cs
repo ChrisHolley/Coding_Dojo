655fa542aca31153b7768b2418dbbb4d7d86bdb1
Christopher Holley <asiholley@gmail.com>
2020-04-06 14:55:32 -0700

    add collections exercise

--- c#/language_essentials/language_essentials/fundamentals/Program.cs
+++ c#/language_essentials/language_essentials/fundamentals/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace fundamentals
 {
@@ -47,6 +48,81 @@ namespace fundamentals
                 {
                     System.Console.WriteLine("Buzz");
                 }
+            }
+
+            // Three Basic Arrays
+            // Create an array to hold integer values 0 through 9
+            int[] numArray = {0,1,2,3,4,5,6,7,8,9};
+            // Create an array of the names "Tim", "Martin", "Nikki", & "Sara"
+            string[] stringArray = {
+                "Tim",
+                "Martin",
+                "Nikki",
+                "Sara"
+            };
+            // Create an array of length 10 that alternates between true and false values, starting with true
+            bool[] boolArray = {
+                true,
+                false,
+                true,
+                false,
+                true,
+                false,
+                true,
+                false,
+                true,
+                false
+            };
+            for (int numInArray = 0; numInArray < numArray.Length; numInArray++)
+            {
+                System.Console.WriteLine(numArray[numInArray]);
+            }
+            for (int stringInArray = 0; stringInArray < stringArray.Length; stringInArray++)
+            {
+                System.Console.WriteLine(stringArray[stringInArray]);
+            }
+            for (int boolInArray = 0; boolInArray < boolArray.Length; boolInArray++)
+            {
+                System.Console.WriteLine(boolArray[boolInArray]);
+            }
+            // List of Flavors 
+            // Make sure to add "using System.Collections.Generic;" at the top of the file
+            // Create a list of ice cream flavors that holds at least 5 different flavors (feel free to add more than 5!)
+            List<string> iceCreamFlavors = new List<string>();
+            iceCreamFlavors.Add("Vanilla");
+            iceCreamFlavors.Add("Chocolate");
+            iceCreamFlavors.Add("Strawberry");
+            iceCreamFlavors.Add("Rocky Road");
+            iceCreamFlavors.Add("Pastachio");
+            for (int iceCreamFlavor = 0; iceCreamFlavor < iceCreamFlavors.Count; iceCreamFlavor++)
+            {
+                System.Console.WriteLine(iceCreamFlavors[iceCreamFlavor]);
+            }
+            // Output the length of this list after building it
+            System.Console.WriteLine("number of ice cream flavors: " + iceCreamFlavors.Count);
+            // Output the third flavor in the list, then remove this value
+            System.Console.WriteLine("flavor 3: " + iceCreamFlavors[2]);
+            iceCreamFlavors.Remove("Strawberry");
+            System.Console.WriteLine("Strawberry flavor deleted" );
+            System.Console.WriteLine("flavor 3: " + iceCreamFlavors[2]);
+
+            // Output the new length of the list (It should just be one fewer!)
+            System.Console.WriteLine("number of ice cream flavors: " + iceCreamFlavors.Count);
+            
+            // User Info Dictionary
+            // Create a dictionary that will store both string keys as well as string values
+            Dictionary<string,string> favoriteIceCream = new Dictionary<string,string>();
+            // Add key/value pairs to this dictionary where:
+            // each key is a name from your names array
+            // each value is a randomly select a flavor from your flavors list.
+            for (int lineInDict = 0; lineInDict < stringArray.Length; lineInDict++)
+            {
+                favoriteIceCream.Add(stringArray[lineInDict], iceCreamFlavors[lineInDict]);
+            }
+            // Loop through the dictionary and print out each user's name and their associated ice cream flavor
+            foreach (var entry in favoriteIceCream)
+            {
+                System.Console.WriteLine(entry.Key + " likes " + entry.Value);
             }
 
         }
