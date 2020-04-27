9fd2d6665ff5c014ac227213dbdf8d3ce650524b
Christopher Holley <asiholley@gmail.com>
2020-04-27 10:31:06 -0700

    add unboxing

---
+++ c#/language_essentials/boxingUnboxing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace boxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var someList = new List<object>
+            {
+                7, 28, -1, true, "chair"
+            };
+            int sum = 0;
+            foreach (var item in someList)
+            {
+                System.Console.WriteLine(item);
+                if (item is int)
+                {
+                    sum += (int)item;
+                }
+                System.Console.WriteLine($"this is the sum: {sum}");
+            }
+        }
+    }
+}
