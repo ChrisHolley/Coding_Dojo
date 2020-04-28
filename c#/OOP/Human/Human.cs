e3dae7c141d00f8fab47404763237c54f0b750da
Christopher Holley <asiholley@gmail.com>
2020-04-28 14:43:00 -0700

    add Human exercise

---
+++ c#/OOP/Human/Human.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Human
+{
+    class Human
+    {
+        public string Name;
+        public int Strength;
+        public int Intelligence;
+        public int Dexterity;
+        private int health;
+
+        // add public gettter for health
+        
+        //add constructor that takes a value to set Name, and set the remaining fields to default values
+
+        // add a constrctor to assign custom values to all fields
+
+        // bluild attack method
+
+        public int Attack(Human target)
+        {
+            
+        }
+    }
+}
