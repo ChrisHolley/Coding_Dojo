3f901f73702bab4954aa92c425768f4003b6f916
Christopher Holley <asiholley@gmail.com>
2020-05-05 13:37:02 -0700

    complete prop_it_up. update gitignore.

---
+++ MERN_Stack/02_React/prop_it_up/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import MyNewComponent from './components/MyNewComponent';
+import PersonCard from './components/PersonCard';
+
+function App() {
+  return (
+    <div className="App">
+      <MyNewComponent someText={"Hello World"}/>
+      <MyNewComponent someText={"What are props?"}/>
+      <MyNewComponent someText={"Because I have no idea"}/>
+      =====================================
+      <PersonCard firstName={"Jane"} lastName={"Doe"} age={45} hairColor={"Black"}/>
+      <PersonCard firstName={"John"} lastName={"Smith"} age={88} hairColor={"Brown"}/>
+      <PersonCard firstName={"Millard"} lastName={"Fillmore"} age={50} hairColor={"Brown"}/>
+      <PersonCard firstName={"Maria"} lastName={"Smith"} age={62} hairColor={"Brown"}/>
+    </div>
+  );
+}
+
+export default App;
