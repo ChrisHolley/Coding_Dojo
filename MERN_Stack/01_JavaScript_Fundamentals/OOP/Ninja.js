c41b528a7681a69e2c42c714fe9be7a9c0540720
Christopher Holley <asiholley@gmail.com>
2020-05-07 15:25:38 -0700

    redo javaScript OOP Ninja ex

--- MERN_Stack/01_JavaScript_Fundamentals/OOP/Ninja.js
+++ MERN_Stack/01_JavaScript_Fundamentals/OOP/Ninja.js
@@ -5,29 +5,28 @@ class Ninja {
         this.speed = 3;
         this.strength = 3;
     }
-    sayName() {
-        console.log(`Hello! I am ninja ${ this.name }!`);
+    sayName = () => console.log(`I an ninja ${this.name}`);
+    showStats = () => {
+        console.log(" ");
+        console.log(`${this.name}'s stats are:`)
+        console.log(`Health: ${this.health}`)
+        console.log(`Speed: ${this.speed}`)
+        console.log(`strength: ${this.strength}`)
+    }
+    drinkSake = () => {
+        console.log(` `);
+        console.log(`${this.name} drank some sake! `);
+        let sakeHealthBoost = 10;
+        this.health += sakeHealthBoost;
+        console.log(`${this.name}'s health has gone up by ${sakeHealthBoost}. Kampai!!!!`)
     }
 
-    showStats() {
-        console.log("======================");
-        console.log(` ${this.name}'s Stats:`);
-        console.log("======================");
-        console.log(` Strength: ${this.strength}`);
-        console.log(` Speed: ${this.speed}`);
-        console.log(` Health: ${this.health}`);
-        
-    }
-
-    drinkSake() {
-        this.health += 10;
-        console.log(` ${this.name}'s health is now ${this.health}. Kanpai!`);
-    }
 }
 
-var Ryu = new Ninja("Ryu"); 
-Ryu.sayName();
-Ryu.showStats();
-Ryu.drinkSake();
 
 
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
+ninja1.showStats();
